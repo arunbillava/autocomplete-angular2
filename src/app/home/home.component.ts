371cdf28c5806b3c30cfe4294d245e50927d2d5a
vishnu k p <vishnukp@vishnus-MacBook-Pro.local>
2017-07-21 17:28:17 +0530

    auto suggestion keyboard control

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Subject } from "rxjs";
 import { AutoCompleteService } from "../services/autocomplete.services";
 import { isNullOrUndefined } from "util";
+
 
 @Component({
   selector: 'home',
@@ -14,16 +15,16 @@ export class HomeComponent implements OnInit {
   searchTerm = new Subject<any>();
   posts: any[] = [];
   autocompleteBox = { hide: true };
-  keyword:string="";
-  selectedPost:any;
-  constructor(private autoCompleteService: AutoCompleteService) {
+  keyword: string = "";
+  selectedPost: any;
+  constructor(private autoCompleteService: AutoCompleteService, private elRef: ElementRef) {
     this.searchTerm.debounceTime(400).distinctUntilChanged().subscribe(searchTerm => {
       console.log(searchTerm);
-       this.selectedPost=null;
+      this.selectedPost = null;
       this.autoCompleteService.getSuggestion(searchTerm).subscribe(response => {
 
         this.posts = response.results;
-        
+
         this.autocompleteBox.hide = false;
 
       }, err => {
@@ -33,27 +34,50 @@ export class HomeComponent implements OnInit {
       });
     });
   }
+  ngAfterViewInit() {
+  }
 
   ngOnInit() {
 
   }
+  arrowkeyLocation = 0;
+
+  onKeyup(event: any, searchText: string) {
 
 
-  onKeyup(searchText: string) {
+
+
+    if (event.keyCode === 40 && this.arrowkeyLocation < this.posts.length - 1) {
+      // Arrow Down
+      this.arrowkeyLocation++;
+    } else if (event.keyCode === 38 && this.arrowkeyLocation > 0) {
+      // Arrow Up
+      this.arrowkeyLocation--;
+    }
+
+
+
+
+
+
+
+
+
+    console.log("code is" + event.keyCode)
 
     if (searchText !== "") {
       this.searchTerm.next(searchText);
-      this.selectedPost=null;
+      this.selectedPost = null;
     }
-    else{
-       this.autocompleteBox.hide = true;
+    else {
+      this.autocompleteBox.hide = true;
     }
 
   }
 
-  settitle(post:any){
-    this.keyword=post.formatted_address;
-    this.selectedPost=post;
+  settitle(post: any) {
+    this.keyword = post.formatted_address;
+    this.selectedPost = post;
     this.autocompleteBox.hide = true;
   }
 }
